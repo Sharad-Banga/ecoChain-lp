501c78897b0a9b69accc47f09a2a131680da779b
Sharad Banga <sharadiit03@gmail.com>
2025-05-24 17:11:37 +0530

    adding collections

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+
+export default function Hero(){
+  return(
+    <>
+        <div className="bg-herox h-screen w-screen">
+
+          hello
+
+        </div>
+    </>
+  )
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
