d36c1d05dc3ac2dc78fa8b787d95c5463028979c
Sharad Banga <sharadiit03@gmail.com>
2025-05-26 19:06:19 +0530

    adding collections

--- app/components/Scroll.tsx
+++ app/components/Scroll.tsx
@@ -3,9 +3,23 @@ export default function Scroll(){
   return (
     <>
 
-    <div className="h-[20vh] bg-fuchsia-200 w-full">
+    <div className="h-[28vh] mt-10 mb-20  w-full flex flex-col justify-evenly items-center">
+
+      <div className="flex justify-center mt-10 text-[20px] text-green-700 font-poppins font-medium">
+        In Collaboration with
+      </div>
+
+      <div className="flex items-center w-[44vw] justify-around align-middle">
+
+        <div className="bg-[url('/images/droplet.png')] h-16 w-16 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/tata.png')] h-16 w-16 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/larsen.png')] h-20 w-28 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/makein.png')] h-16 w-16 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/pyramid.png')] h-16 w-16 bg-center bg-cover"></div>
+      </div>
 
     </div>
+
     
     </>
   )
