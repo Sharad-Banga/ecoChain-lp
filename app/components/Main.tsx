5bd4a7e6fb719f7bee7ad726ad95cbf12bd72fd8
Sharad Banga <sharadiit03@gmail.com>
2025-05-28 05:03:04 +0530

    adding collections

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -108,7 +108,7 @@ export default function Main(){
 
                             <div className="h-[22%] w-[74%] bg-[#FFFFFF] rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
                                 <div className="h-[26px] w-[26px] bg-[#D9D9D9] rounded-sm flex justify-center items-center ml-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
                                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
                                     </svg>
 
--- app/components/Scroll.tsx
+++ app/components/Scroll.tsx
@@ -3,19 +3,19 @@ export default function Scroll(){
   return (
     <>
 
-    <div className="h-[28vh] mt-10 mb-20  w-full flex flex-col justify-evenly items-center">
+    <div className="h-[14vh] sm:h-[28vh]  mt-2 sm:mt-10 mb-20  w-full flex flex-col justify-evenly items-center">
 
-      <div className="flex justify-center mt-10 text-[20px] text-green-700 font-poppins font-medium">
+      <div className="flex justify-center sm:mt-10 text-[16px] sm:text-[20px] text-green-700 font-poppins font-medium">
         In Collaboration with
       </div>
 
-      <div className="flex items-center w-[44vw] justify-around align-middle">
+      <div className="flex items-center w-[80vw] sm:w-[44vw] justify-around align-middle">
 
-        <div className="bg-[url('/images/droplet.png')] h-16 w-16 bg-center bg-cover"></div>
-        <div className="bg-[url('/images/tata.png')] h-16 w-16 bg-center bg-cover"></div>
-        <div className="bg-[url('/images/larsen.png')] h-20 w-28 bg-center bg-cover"></div>
-        <div className="bg-[url('/images/makein.png')] h-16 w-16 bg-center bg-cover"></div>
-        <div className="bg-[url('/images/pyramid.png')] h-16 w-16 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/droplet.png')] h-8 w-8 sm:h-16 sm:w-16 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/tata.png')] h-8 w-8 sm:h-16 sm:w-16  bg-center bg-cover"></div>
+        <div className="bg-[url('/images/larsen.png')] h-10 w-14 sm:h-20 sm:w-28 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/makein.png')] h-8 w-8 sm:h-16 sm:w-16 bg-center bg-cover"></div>
+        <div className="bg-[url('/images/pyramid.png')] h-8 w-8 sm:h-16 sm:w-16 bg-center bg-cover"></div>
       </div>
 
     </div>
