d15f662b2b50db0be4e0aee578fdb2564eb7668d
Sharad Banga <sharadiit03@gmail.com>
2025-05-28 23:24:34 +0530

    adding collections

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -2,20 +2,21 @@ export default function Main(){
   return(
     <>
 
-    <div className="main h-[270vh] w-full flex flex-col items-center  bg-white justify-around">
+    <div className="main h-[240vh] sm:h-[270vh] w-full flex flex-col items-center  bg-white justify-between sm:justify-around">
 
 
-          <div className="works h-[40vh] w-[80vw]  flex justify-evenly items-center">
-            <div className="  h-[70%] w-[22vw] flex flex-col justify-evenly">
-              <div className="text-[30px] font-raleway font-semibold text-[#1B5E20]"> 
+          <div className="works h-[40vh] w-[80vw] flex-col-reverse  sm:flex-row flex justify-evenly items-center">
+            <div className="  h-[70%] w-[70vw] sm:w-[22vw] flex flex-col justify-evenly mt-8 sm:mt-0">
+              <div className="text-[24px] sm:text-[30px] font-raleway font-semibold text-[#1B5E20] mb-2 sm:mb-0"> 
                 How Eco Chain Works
+                
               </div>
-              <div className="text-wrap text-[17px] opacity-60 font-poppins font-extralight">
+              <div className="text-wrap text-[14px] flex items-center sm:text-[17px] opacity-60 font-poppins font-extralight">
                 A quick glance at your journey from recycling to rewarding. Eco Chain simplifies sustainable living into four easy steps, powered by AI and community collaboration.
               </div>
             </div>
-            <div className="h-full w-[22vw] bg-white bg-center rounded-lg drop-shadow-2xl  ">
-              <div className=" h-full w-full bg-center pt-5 pb-5  bg-[url('/images/gradient2.png')] rounded-lg flex flex-col justify-evenly items-center">
+            <div className="sm:h-full w-[70vw] h-[70vh] sm:w-[22vw] bg-white bg-center rounded-lg drop-shadow-2xl  ">
+              <div className=" sm:h-full h-[40vh] w-full bg-center pt-5 pb-5  bg-[url('/images/gradient2.png')] rounded-lg flex flex-col justify-evenly items-center">
 
                   <div className="h-[20%] w-[74%] bg-white rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
                       <div className="h-[38px] w-[38px] bg-[#53e2645b] rounded-md flex justify-center items-center ml-2">
@@ -23,7 +24,7 @@ export default function Main(){
                               <path d="M 11.03125 3 C 9.8865255 3 8.8219757 3.6090929 8.28125 4.6152344 L 8.2734375 4.6289062 L 7.8125 5.5527344 A 1.0001 1.0001 0 1 0 9.5996094 6.4472656 L 10.042969 5.5625 L 10.044922 5.5625 C 10.222484 5.2334243 10.598621 5 11.03125 5 L 13.019531 5 C 13.453413 5 13.83195 5.2321005 14.009766 5.5644531 L 14.019531 5.5820312 L 15.611328 8.2910156 L 14.251953 9.0664062 C 13.864953 9.2874063 13.937188 9.8644219 14.367188 9.9824219 L 18.017578 10.982422 C 18.283578 11.055422 18.557859 10.898813 18.630859 10.632812 L 19.630859 6.9824219 C 19.748859 6.5524219 19.287391 6.1960156 18.900391 6.4160156 L 17.349609 7.3007812 L 15.773438 4.6210938 C 15.233254 3.6114463 14.165649 3 13.019531 3 L 11.03125 3 z M 6.8203125 9.0058594 L 3.0859375 9.6054688 C 2.6459375 9.6764687 2.5110938 10.244859 2.8710938 10.505859 L 4.2246094 11.484375 L 2.3847656 14.59375 L 2.375 14.611328 C 1.8464894 15.596074 1.9302039 16.807596 2.5859375 17.714844 L 2.5859375 17.716797 L 3.3125 18.71875 C 3.8991007 19.53189 4.8493769 20 5.8535156 20 L 8 20 A 1.0001 1.0001 0 1 0 8 18 L 5.8535156 18 C 5.4736544 18 5.1289929 17.819689 4.9335938 17.548828 L 4.2070312 16.544922 C 3.9867648 16.24017 3.9651824 15.881848 4.1386719 15.558594 L 5.8496094 12.660156 L 7.2050781 13.640625 C 7.5660781 13.901625 8.0631875 13.59625 7.9921875 13.15625 L 7.3925781 9.421875 C 7.3495781 9.148875 7.0933125 8.9628594 6.8203125 9.0058594 z M 19.960938 12.988281 A 1.0001 1.0001 0 0 0 19.183594 14.576172 L 19.888672 15.574219 C 20.050275 15.882608 20.027854 16.251837 19.816406 16.546875 L 19.820312 16.544922 L 19.089844 17.554688 C 18.898844 17.819209 18.550743 18 18.169922 18 L 15 18 L 15 16.509766 C 15 16.056766 14.451859 15.829391 14.130859 16.150391 L 11.640625 18.640625 C 11.441625 18.839625 11.441625 19.160375 11.640625 19.359375 L 14.130859 21.849609 C 14.451859 22.170609 15 21.943234 15 21.490234 L 15 20 L 18.169922 20 C 19.169101 20 20.121946 19.540134 20.710938 18.724609 L 21.441406 17.714844 L 21.443359 17.712891 C 22.089804 16.810875 22.183361 15.605037 21.650391 14.615234 L 21.621094 14.5625 L 20.816406 13.423828 A 1.0001 1.0001 0 0 0 19.960938 12.988281 z"></path>
                           </svg>
                       </div>
-                      <div className="font-poppins text-[20px] text-[#002D07] ml-2">
+                      <div className="font-poppins text-[14px] sm:text-[20px] text-[#002D07] ml-2">
                             Recycle
                       </div>
                   </div>
@@ -33,7 +34,7 @@ export default function Main(){
 
                           </div>
                       </div>
-                      <div className="font-poppins text-[18px] text-[#002D07] ml-2">
+                      <div className="font-poppins text-[14px] sm:text-[18px] text-[#002D07] ml-2">
                             Earn Eco Tokens
                       </div>
                   </div>
@@ -43,7 +44,7 @@ export default function Main(){
 
                           </div>
                       </div>
-                      <div className="font-poppins text-[16px] text-[#002D07] ml-2">
+                      <div className="font-poppins text-[12px] sm:text-[16px] text-[#002D07] ml-2">
                             Sell Upcycled Products
                       </div>
                   </div>
@@ -71,10 +72,10 @@ export default function Main(){
 
 
 
-          <div className="works h-[45vh] w-[75vw]  flex justify-evenly items-center ">
+          <div className="works h-[80vh] sm:h-[45vh] w-[75vw]  flex flex-col sm:flex-row justify-evenly items-center ">
 
 
-              <div className="h-full w-[25vw] bg-white bg-center rounded-lg drop-shadow-2xl ">
+              <div className="h-[82vh] sm:h-full w-[70vw] sm:w-[25vw] bg-white bg-center rounded-lg drop-shadow-2xl ">
                 <div className="h-full w-full bg-center pt-5   bg-[url('/images/gradient2.png')] rounded-lg flex flex-col justify-end items-center ">
 
                       <div className="h-[88%] w-[60%] bg-white rounded-t-2xl border-[#8C8C8C] border-3 border-b-0">
@@ -100,20 +101,20 @@ export default function Main(){
                           <div className="flex flex-col items-center justify-evenly h-[20%] pt-1">
 
                             <div className="font-poppins font-medium text-[18px]">Nearby  Center</div>
-                            <div className="font-poppins font-extralight text-[6px] opacity-90"> Drop off your recyclable waste at a nearby partner center</div>
+                            <div className="font-poppins font-extralight text-[4px] sm:text-[6px] opacity-90"> Drop off your recyclable waste at a nearby partner center</div>
 
                           </div>
 
                           <div className="flex flex-col items-center justify-evenly  h-[77%]">
 
-                            <div className="h-[22%] w-[74%] bg-[#FFFFFF] rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
+                            <div className="h-[22%] w-[74%] bg-[#FFFFFF] rounded-sm sm:rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
                                 <div className="h-[26px] w-[26px] bg-[#D9D9D9] rounded-sm flex justify-center items-center ml-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
                                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
                                     </svg>
 
                                 </div>
-                                <div className="font-poppins text-[10px] text-[#002D07] ml-2">
+                                <div className="font-poppins text-[8px] sm:text-[10px] text-[#002D07] ml-2">
                                      <div>
                                           Raj Enterprises
                                      </div>
@@ -126,13 +127,13 @@ export default function Main(){
 
 
 
-                            <div className="h-[22%] w-[74%] bg-[#FFFFFF] rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
+                            <div className="h-[22%] w-[74%] bg-[#FFFFFF] rounded-sm sm:rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
                                 <div className="h-[26px] w-[26px] bg-[#D9D9D9] rounded-sm  flex justify-center items-center ml-1">
                                     <div className="group-hover: transition-transform bg-[url('/images/bin.png')] bg-cover w-[80%] h-[60%]">
 
                                     </div>
                                 </div>
-                                <div className="font-poppins text-[10px] text-[#002D07] ml-2">
+                                <div className="font-poppins text-[7px] sm:text-[10px] text-[#002D07] ml-2">
                                       <div>
                                           Wasi Manufacturer
                                      </div>
@@ -145,14 +146,14 @@ export default function Main(){
 
 
 
-                            <div className="h-[22%] w-[74%] bg-[#FFFFFF] rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
+                            <div className="h-[22%] w-[74%] bg-[#FFFFFF] rounded-sm sm:rounded-lg drop-shadow-xl flex items-center justify-start hover:scale-105 transition-transform duration-100 group">
                                 <div className="h-[26px] w-[26px] bg-[#D9D9D9] rounded-sm flex justify-center items-center ml-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
                                       <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
                                     </svg>
 
                                 </div>
-                                <div className="font-poppins text-[10px] text-[#002D07] ml-2">
+                                <div className="font-poppins text-[8px] sm:text-[10px] text-[#002D07] ml-2">
                                     <div>
                                           Nitin Exports
                                      </div>
@@ -175,12 +176,12 @@ export default function Main(){
 
 
 
-              <div className="  h-[90%] w-[25vw] flex flex-col justify-evenly ">
-                <div className="text-[30px] font-raleway font-semibold text-[#1B5E20]"> 
+              <div className="  h-[90%]  w-[80vw] sm:w-[25vw] flex flex-col justify-evenly items-center sm:items-start">
+                <div className="text-[20px] sm:text-[30px] font-raleway font-semibold text-[#1B5E20]"> 
                       Recycle Waste            
                       <br />
                 </div>
-                <div className="text-wrap text-[17px] opacity-60 font-poppins font-extralight">
+                <div className="text-wrap text-[13px] sm:text-[17px] opacity-60 flex flex-col   font-poppins font-extralight">
                       Drop off your recyclable waste at a nearby partner center. Use our AI assistant to guide you through the segregation process, then upload a photo or scan the QR code at the drop-off point.                    <br />
                       <br />
 
@@ -223,13 +224,13 @@ export default function Main(){
 
 
 
-          <div className="works h-[40vh] w-[80vw]  flex justify-evenly items-center ">
-            <div className="  h-[90%] w-[25vw] flex flex-col justify-evenly ">
+          <div className="works  h-[40vh] w-[80vw]  flex flex-col-reverse sm:flex-row justify-start sm:justify-evenly items-center ">
+            <div className="  h-[90%] w-[70vw] mt-4 sm:mt-0 sm:w-[25vw] flex flex-col justify-evenly ">
               <div className="text-[30px] font-raleway font-semibold text-[#1B5E20]"> 
                 Earn Eco Tokens
                 <br />
               </div>
-              <div className="text-wrap text-[17px] opacity-60 font-poppins font-extralight">
+              <div className="text-wrap text-[13px] sm:text-[17px] opacity-60 font-poppins font-extralight">
                     These tokens recognize your contribution to the planet and fuel your journey in the Eco Chain ecosystem.
                     <br />
                     <br />
@@ -242,25 +243,25 @@ export default function Main(){
 
               </div>
             </div>
-            <div className="h-full w-[24vw] bg-white bg-center rounded-lg drop-shadow-2xl group">
+            <div className="h-full w-[75vw]  sm:w-[24vw] bg-white bg-center rounded-lg drop-shadow-2xl group">
               <div className="h-full w-full bg-center pt-5 pb-5  bg-[url('/images/gradient2.png')] rounded-lg flex flex-col justify-evenly items-center">
 
                   <div className="h-[95%] w-[76%] bg-white flex flex-col justify-evenly items-center rounded-2xl">
-                        <div className="font-raleway font-light mt-4">
+                        <div className="font-raleway font-light mt-2 sm:mt-4">
                           Congratulations <span className="group-hover:animate-ping">🎉</span>
                         </div>
-                        <div className="font-raleway font-medium text-[20px] text-[#1B5E20]">
+                        <div className="font-raleway font-medium text-[16px] sm:text-[20px] text-[#1B5E20]">
                           You won 10 Eco Tokens!
                         </div>
                         <div className="flex justify-evenly items-center w-[60%] mb-5 mt-3">
-                          <div className="font-poppins text-[#02710f] text-[32px] font-bold">+10</div>
-                          <div className="h-[85px] group-hover:scale-110 transition-transform w-[85px] bg-[#CCFFD2] rounded-full flex justify-center items-center text-[30px]">
+                          <div className="font-poppins text-[#02710f] text-[20px] sm:text-[32px] font-bold">+10</div>
+                          <div className="sm:h-[85px] group-hover:scale-110 transition-transform h-[55px] w-[55px] sm:w-[85px] bg-[#CCFFD2] rounded-full flex justify-center items-center text-[30px]">
                                   ☘️
                           </div>
                         </div>
                         <div className="flex justify-evenly items-center gap-3 mb-4">
-                          <div className="pl-2 pr-2 pt-1 pb-1 font-poppins text-[14px] cursor-pointer hover:scale-115 transition-transform bg-[#00AA14] text-white rounded-lg drop-shadow-2xl">Redeem Now</div>
-                          <div className="pl-3 pr-3 pt-1 pb-1 font-poppins text-[14px] bg-[#FF6767]  text-white rounded-lg drop-shadow-3xl drop-shadow-black hover:scale-115 cursor-pointer transition-transform">Share </div>
+                          <div className="pl-2 pr-2 pt-1 pb-1 font-poppins text-[11px] sm:text-[14px] cursor-pointer hover:scale-115 transition-transform bg-[#00AA14] text-white rounded-lg drop-shadow-2xl">Redeem Now</div>
+                          <div className="pl-3 pr-3 pt-1 pb-1 font-poppins text-[11px] sm:text-[14px] bg-[#FF6767]  text-white rounded-lg drop-shadow-3xl drop-shadow-black hover:scale-115 cursor-pointer transition-transform">Share </div>
                         </div>
                   </div>
 
