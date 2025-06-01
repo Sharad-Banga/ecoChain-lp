ad1bf6396df0d638b2def24b8310353ccaebb861
Sharad Banga <sharadiit03@gmail.com>
2025-06-01 19:25:55 +0530

    adding collections

--- app/components/ThreeR.tsx
+++ app/components/ThreeR.tsx
@@ -3,7 +3,7 @@ export default function ThreeR(){
 
   return(
     <>
-        <div className="flex overflow-hidden justify-evenly items-center mt-12 sm:mt-0 h-[8vh] md:h-[22vh] w-full font-medium  bg-center text-[#1B5E20] text-[20px] sm:text-[25px] md:text-[50px] font-poppins border-t-1 border-b-1 border-black/10">
+        <div className="flex overflow-hidden justify-evenly items-center mt-12 sm:mt-0 h-[8vh] md:h-[22vh] w-full font-medium  bg-center text-[#1B5E20] text-[20px] sm:text-[25px] md:text-[50px] font-poppins border-t-1 border-b-1 border-black/10 bg-black/5">
           
               <div className="w-full animate-scroll  md:w-[80vw] flex justify-evenly items-center">
                       <div >
